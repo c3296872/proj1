bfa4570eff4c30123dc38157d8a4b20c43b892a5
Jesse Cramp <jesse.cramp@uon.edu.au>
2019-04-04 08:43:22 +0000

    Input initialised

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
 ///// afbgdkgbhjd
+int key;
+int text[26];
+for(int n = 0; n<26; n++)
+{
+    text[n] = n;
+    printf("%d\n", text[n]);
+}
 
-int x;
 
-float y;
 
-x = y + 1;
+
   return 0;
   }
