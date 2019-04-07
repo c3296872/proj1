c73e7bd2460a04ebe6f2f4b1e2a921934c735db7
c3296872 <48542261+c3296872@users.noreply.github.com>
2019-04-07 17:05:53 +1000

    Rotation cipher/decypher
    Currently only works for 1 word

--- main.c
+++ main.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 
-int main() 
+
+void main() 
 {
-///// afbgdkgbhjd
-int key;
-int text[26];
-for(int n = 0; n<26; n++)
+char text[200]; //string input
+char rotated[200];   // array stores encyrpted text
+int n;  // n is size of string
+int K = 5; //K is rotation cipher encryption "key"
+printf("Enter text to be encrypted: ");
+scanf("%s", text);
+
+//printf("\n please enter encryption key: ");
+//scanf("%d", K);
+for(n = 0; text[n] != NULL; n++)
 {
-    text[n] = n;
-    printf("%d\n", text[n]);
+    rotated[n] = text[n] + K;
+}
+rotated[n+1] = NULL;
+printf("%s\n", rotated);
+
+for(n = 0; text[n] != 0; n++)
+{
+    rotated[n] = rotated[n] - K;
 }
 
+printf("%s", rotated);
 
 
 
-  return 0;
-  }
+}
