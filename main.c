cbda94ec11031c65f9b57d4e3b340f3fc262ac56
Jesse Cramp <jesse.cramp@uon.edu.au>
2019-04-22 03:22:04 +0000

    Rotation decryption with known key

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /*
-char RotationEncryption(void);
+int RotationEncryption(void);
+int RotationDecryption(void);
 
 int main()
 {
@@ -32,8 +33,9 @@ int main()
     
 }
 */
-//char RotationEncryption(void) 
-void main()
+/*
+int RotationEncryption(void) 
+
 {
     char text[] = "The 5 Quick Brown Fox's Winter Coats are coming in!"; //array stores input
     char rotated[200];   // array stores encyrpted text
@@ -67,11 +69,42 @@ void main()
     printf("Encrypted text: %s\n", rotated); // prints encrypted text
     
 }
+*/
+//int RotationDecryption(void)
+int main()
+{
+    char text[] = "YMj 5 VZnHP gWTBS kTC'x BnSYJW HtFYX FwJ HtRNSL Ns!"; //array stores input
+    char rotated[200];   // array stores decyrpted text
+    int n = 0;  // n + 1 = size of string
+    int K = 5; //K is rotation cipher encryption "key"
+    for(n = 0; text[n] != 0; n++) //for loop converts any lower case to upper case
+    {
+        if(text[n] > 96 && text[n] < 123) //if statement checks if character is lower case before converting
+        {
+            text[n] = text[n] - 32; // expression converts lower case letter to uppercase as per ASCII table
+        }
+    }
+    printf("Text to be decrypted: %s\n", text); // prints text to be decrypted
+    for(int x = 0; text[x] != 0; x++) //for loop decrypts text
+    {
+        if(text[x] > 64 && text[x] < 91) //if statement checks if character is a letter before encrypting
+        {
+            rotated[x] = text[x] - K; //expression decrypts character using key and stores it in decrypted text array
+            if(rotated[x] < 65) //if statement checks if encrypted character is a letter
+            {
+                rotated[x] = rotated[x] + 26; //expression converts non-letter characters to correct letter
+            }
+        }
+        else
+        {
+            rotated[x] = text[x]; //expression copies non-letter characters without decrypting
+        }
 
+    }
+    printf("Decrypted text: %s", rotated);
+}
 
-
-
-
+// YMJ 5 VZNHP GWTBS KTC'X BNSYJW HTFYX FWJ HTRNSL NS!
 
 
 
